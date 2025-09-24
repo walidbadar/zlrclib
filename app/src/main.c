230f6d9939850ed4dc1ad93987906df0c8e0cc4c
Muhammad Waleed Badar <walid.badar@gmail.com>
2025-09-24 15:29:23 +0500

    Add config for native_sim
    Signed-off-by: Muhammad Waleed Badar <walid.badar@gmail.com>

--- app/src/main.c
+++ app/src/main.c
@@ -13,21 +13,17 @@ LOG_MODULE_REGISTER(requests_sample);
 
 #define ARTIST "benson+boone"
 #define TRACK  "mystical+magical"
-#define TRACK1 "beautiful+things"
 
 #if defined(CONFIG_NET_SOCKETS_SOCKOPT_TLS)
-const char *url[] = {"https://lrclib.net/api/get?artist_name=" ARTIST "&track_name=" TRACK,
-		     "https://lrclib.net/api/get?artist_name=" ARTIST "&track_name=" TRACK1};
+const char *url = "https://lrclib.net/api/get?artist_name=" ARTIST "&track_name=" TRACK;
 #else
-const uint8_t url[] = {
-	"http://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature"};
+const uint8_t *url = "http://google.com/";
 #endif
 
 static int resp_cb(struct http_response *rsp, enum http_final_call final_data,
 			    void *user_data)
 {
-	printk("%.*s", rsp->data_len, rsp->body_frag_start);
-
+	LOG_HEXDUMP_INF(rsp->body_frag_start, rsp->body_frag_len, "RECV BUFFER");
 	return 0;
 }
 
@@ -41,13 +37,11 @@ int main(void)
 	struct requests_ctx ctx;
 
 	while (1) {
-		for (size_t i = 0; i < 2; i++) {
-			ret = requests_get(&ctx, resp_cb, url[i]);
-			if (ret < 0) {
-				LOG_ERR("Requests GET failed: %d", ret);
-			}
-			k_msleep(60 * MSEC_PER_SEC);
+		ret = requests_get(&ctx, resp_cb, url);
+		if (ret < 0) {
+			LOG_ERR("Requests GET failed: %d", ret);
 		}
+		k_msleep(60 * MSEC_PER_SEC);
 	}
 
 	return 0;
