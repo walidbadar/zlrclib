016819eca4e5ae4b594604266389e514da959be8
Muhammad Waleed Badar <walid.badar@gmail.com>
2025-10-15 17:58:30 +0500

    Added intial support for display
    Signed-off-by: Muhammad Waleed Badar  <walid.badar@gmail.com>

---
+++ app/src/display/zlrclib_display.c
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2025 Muhammad Waleed Badar
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <lvgl.h>
+#include <zephyr/drivers/display.h>
+#include <app/zlrclib_display.h>
+
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(zlrclib_display);
+
+static const struct device *ddev;
+static lv_obj_t *display_title;
+static lv_obj_t *lyrics;
+
+void zlrclib_display_lyrics(uint8_t *synced_lyrics)
+{
+	lv_label_set_text(lyrics, synced_lyrics);
+	lv_task_handler();
+}
+
+static int zlrclib_display(void)
+{
+	ddev = DEVICE_DT_GET(DT_CHOSEN(zephyr_display));
+	if (!device_is_ready(ddev)) {
+		LOG_ERR("Display device not ready");
+		return -ENODEV;
+	}
+
+   display_blanking_off(ddev);
+
+	display_title = lv_label_create(lv_scr_act());
+	lv_obj_align(display_title, LV_ALIGN_TOP_MID, 0, 0);
+	lv_label_set_text(display_title, "ZLRCLIB APP");
+
+	lyrics = lv_label_create(lv_scr_act());
+	lv_obj_align(lyrics, LV_ALIGN_TOP_LEFT, 0, 14);
+   lv_label_set_text(lyrics, "");
+
+	return 0;
+}
+
+SYS_INIT(zlrclib_display, APPLICATION, 95);
---
+++ include/app/zlrclib_display.h
@@ -0,0 +1,12 @@
+#ifndef ZLRCLIB_DISPLAY_H_
+#define ZLRCLIB_DISPLAY_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <limits.h>
+
+void zlrclib_display_lyrics(uint8_t *synced_lyrics);
+
+#endif /* ZLRCLIB_DISPLAY_H_ */
